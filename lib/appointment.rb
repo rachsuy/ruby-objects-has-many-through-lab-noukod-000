a3350b1050b1bea7b1520eda9b6cee73b8ebbdd1
Rachele Suy <rachelesuy@gmail.com>
2019-05-23 05:13:12 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,31 +1,25 @@
 class Appointment
-  attr_accessor :name, :doctor, :date 
-  attr_reader :name, :doctor, :date 
- 
-  @@all = []
-  
-  def initialize(name, doctor, date)
-    @doctor = doctor
-    @patient = patient 
-    @date = date
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def patient
-    self.name
-  end
-  
-  def patients
-    self.patient
-    
-  end
-  
-  def doctors
-    self.doctor
-  end
-    
-end
+
+    attr_reader :name, :doctor, :date
+
+    @@all = []
+
+    def initialize(name,doctor,date)
+        @name = name
+        @doctor = doctor
+        @date = date
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def patient
+        self.name
+    end
+
+    def doctors
+        self.doctor
+    end
+
